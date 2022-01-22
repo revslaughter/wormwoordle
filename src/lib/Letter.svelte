a7df575bd0e9ce3c2d22de39bd7642af57d57638
James Slaughter <jmslaughter85@outlook.com>
2022-01-22 15:06:21 -0600

    initial mock-up

---
+++ src/lib/Letter.svelte
@@ -0,0 +1,51 @@
+<script>
+	export let char = '';
+	export let status = 'new';
+
+	if (char === '') {
+		status = 'dead';
+	}
+</script>
+
+<div class="letterTile {status}">{char}</div>
+
+<style>
+	.letterTile {
+		display: grid;
+		grid-row: 1;
+		grid-column: auto;
+
+		justify-content: center;
+		align-items: center;
+
+		border-bottom: 1px solid black;
+		border-radius: 15%;
+
+		box-shadow: 0px 0.125em 0.125em rgb(177, 177, 177);
+
+		width: 2em;
+		height: 2em;
+
+		text-transform: uppercase;
+		font-family: Helvetica, 'Segoe UI', sans-serif;
+	}
+
+	.new {
+		background-color: rgb(218, 218, 218);
+	}
+	.close {
+		background-color: rgb(236, 236, 83);
+	}
+	.wrong {
+		background-color: rgb(65, 65, 65);
+		color: whitesmoke;
+	}
+	.right {
+		background-color: rgb(95, 190, 95);
+	}
+	.dead {
+		background-color: rgb(233, 233, 233);
+		border-radius: 10%;
+		border-bottom: 2px solid rgb(218, 218, 218);
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,77 @@
+<script>
+	import Letter from '$lib/Letter.svelte';
+</script>
+
+<div class="wordRow">
+	<Letter char="a" status="right" />
+	<Letter char="c" status="wrong" />
+	<Letter char="r" status="close" />
+	<Letter char="o" status="close" />
+	<Letter char="b" status="close" />
+	<Letter char="a" status="close" />
+	<Letter char="t" status="close" />
+	<Letter char="➟" />
+	<Letter />
+	<Letter />
+	<Letter />
+	<Letter />
+</div>
+
+<div class="wordRow">
+	<Letter char="a" status="right" />
+	<Letter char="b" status="right" />
+	<Letter char="b" status="right" />
+	<Letter char="a" status="close" />
+	<Letter char="➟" />
+	<Letter />
+	<Letter />
+	<Letter />
+	<Letter />
+	<Letter />
+	<Letter />
+	<Letter />
+</div>
+
+<div class="wordRow">
+	<Letter char="t" status="wrong" />
+	<Letter char="o" status="close" />
+	<Letter char="y" status="wrong" />
+	<Letter char="➟" />
+	<Letter />
+	<Letter />
+	<Letter />
+	<Letter />
+	<Letter />
+	<Letter />
+	<Letter />
+	<Letter />
+</div>
+
+<div class="wordRow">
+	<Letter char="a" status="right" />
+	<Letter char="b" status="right" />
+	<Letter char="b" status="right" />
+	<Letter char="r" status="right" />
+	<Letter char="e" status="right" />
+	<Letter char="v" status="right" />
+	<Letter char="i" status="right" />
+	<Letter char="a" status="right" />
+	<Letter char="t" status="right" />
+	<Letter char="i" status="right" />
+	<Letter char="o" status="right" />
+	<Letter char="n" status="right" />
+</div>
+
+<style>
+	.wordRow {
+		width: 80%;
+		margin-top: 0.5em;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 0.5em;
+		display: grid;
+
+		font-size: xx-large;
+		grid-template-columns: repeat(12, 12fr);
+	}
+</style>
