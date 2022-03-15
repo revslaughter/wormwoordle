f3183a6cb7313889844220152d6165cff18d7c1b
James Slaughter <jmslaughter85@outlook.com>
2022-03-14 20:46:33 -0500

    Added a modal explaining the rules

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,21 +1,7 @@
-<div class="intro">
-	<h1>WormWoodle 👹</h1>
-	<h2>The variable-length <a href="https://www.powerlanguage.co.uk/wordle/">Wordle</a> clone</h2>
-	<p>Take a guess, up to 10 letters!</p>
-	<p>We'll let you know if it's too long or too short</p>
-</div>
-<hr />
+<script>
+</script>
 
 <slot />
 
 <style>
-	.intro {
-		max-width: 30rem;
-		margin-left: auto;
-		margin-right: auto;
-	}
-	hr {
-		margin-top: 1.5em;
-		margin-bottom: 2em;
-	}
 </style>
